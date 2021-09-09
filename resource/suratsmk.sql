d1eae36a70a05570aa254957e246d840e5f2b65f
Ida Bagus Widiantara <kecialpou@gmail.com>
2021-09-09 14:15:09 +0800

    project

---
+++ resource/suratsmk.sql
@@ -0,0 +1,319 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 08 Sep 2021 pada 02.39
+-- Versi server: 5.7.24
+-- Versi PHP: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `suratsmk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_disposisi`
+--
+
+CREATE TABLE `tbl_disposisi` (
+  `id_disposisi` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `isi_disposisi` mediumtext NOT NULL,
+  `sifat` varchar(100) NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatan` varchar(250) NOT NULL,
+  `id_surat` int(10) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_instansi`
+--
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `kepsek` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_instansi`
+--
+
+INSERT INTO `tbl_instansi` (`id_instansi`, `institusi`, `nama`, `status`, `alamat`, `kepsek`, `nip`, `website`, `email`, `logo`, `id_user`) VALUES
+(1, 'SMK PARIWISATA KOTA MATARAM', 'SMK PARIWISATA', 'SEKOLAH MENENGAH KEJURUAN', 'Jl. Bima Sena No.1, Cilinaya, Kec. Cakranegara, Kota Mataram, Nusa Tenggara Bar. 83239', 'Ir. I KOMANG RENA, SE.,M.Sc.,MPd', '-', 'http://ntb.kemenag.go.id', 'kemenag@gmail.com', 'download.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_klasifikasi`
+--
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(5) NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `uraian` mediumtext NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_klasifikasi`
+--
+
+INSERT INTO `tbl_klasifikasi` (`id_klasifikasi`, `kode`, `nama`, `uraian`, `id_user`) VALUES
+(1, '420', 'PENDIDIKAN', 'PENDIDIKAN', 1),
+(2, '420.1', 'Pendidikan Khusus Klasifikasi disini Pendidikan Putra/I Irja', 'Pendidikan Khusus Klasifikasi disini Pendidikan Putra/I Irja', 1),
+(3, '421', 'Sekolah', 'Sekolah', 1),
+(4, '421.1', 'Pra Sekolah', 'Pra Sekolah', 1),
+(5, '421.2', 'Sekolah Dasar', 'Sekolah Dasar', 1),
+(6, '421.3', 'Sekolah Menengah', 'Sekolah Menengah', 1),
+(7, '421.4', 'Sekolah Tinggi', 'Sekolah Tinggi', 1),
+(8, '421.5', 'Sekolah Kejuruan', 'Sekolah Kejuruan', 1),
+(9, '421.6', 'Kegiatan Sekolah, Dies Natalis Lustrum', 'Kegiatan Sekolah, Dies Natalis Lustrum', 1),
+(10, '421.7', 'Kegiatan Pelajar', 'Kegiatan Pelajar', 1),
+(11, '421.71', 'Reuni Darmawisata', 'Reuni Darmawisata', 1),
+(12, '421.72', 'Pelajar Teladan', 'Pelajar Teladan', 1),
+(13, '421.73', 'Resimen Mahasiswa', 'Resimen Mahasiswa', 1),
+(14, '421.8', 'Sekolah Pendidikan Luar Biasa', 'Sekolah Pendidikan Luar Biasa', 1),
+(15, '421.9', 'PLS / Pemberantasan Buta Huruf', 'PLS / Pemberantasan Buta Huruf', 1),
+(16, '422', 'Administrasi Sekolah', 'Administrasi Sekolah', 1),
+(17, '422.1', 'Persyaratan Masuk Sekolah, Testing, Ujian,Pendaftaran, Mapras', 'Persyaratan Masuk Sekolah, Testing, Ujian,Pendaftaran, Mapras', 1),
+(18, '422.2', 'Tahun Pelajaran', 'Tahun Pelajaran', 1),
+(19, '422.3', 'Hari Libur', 'Hari Libur', 1),
+(20, '422.4', 'Uang Sekolah, Klasifikasi Disini SPP', 'Uang Sekolah, Klasifikasi Disini SPP', 1),
+(21, '422.5', 'Beasiswa', 'Beasiswa', 1),
+(22, '423', 'Metode Belajar', 'Metode Belajar', 1),
+(23, '423.1', 'Kuliah', 'Kuliah', 1),
+(24, '423.2', 'Ceramah, Simposium', 'Ceramah, Simposium', 1),
+(25, '423.3', 'Diskusi', 'Diskusi', 1),
+(26, '423.4', 'Kuliah Lapangan, Widyawisata, KKN, Studi Tur', 'Kuliah Lapangan, Widyawisata, KKN, Studi Tur', 1),
+(27, '423.5', 'Kurikulum', 'Kurikulum', 1),
+(28, '423.6', 'Karya Tulis', 'Karya Tulis', 1),
+(29, '423.7', 'Ujian', 'Ujian', 1),
+(30, '424', 'Tenaga Pengajar, Guru, Dosen, Dekan, Rektor', 'Tenaga Pengajar, Guru, Dosen, Dekan, Rektor', 1),
+(31, '425', 'Sarana Pendidikan', 'Sarana Pendidikan', 1),
+(32, '425.1', 'Gedung', 'Gedung', 1),
+(33, '425.11', 'Gedung Sekolah', 'Gedung Sekolah', 1),
+(34, '425.12', 'Kampus', 'Kampus', 1),
+(35, '425.13', 'Pusat Kegiatan Mahasiswa', 'Pusat Kegiatan Mahasiswa', 1),
+(36, '425.2', 'Buku', 'Buku', 1),
+(37, '425.3', 'Perlengkapan Sekolah', 'Perlengkapan Sekolah', 1),
+(38, '426', 'Keolahragaan', 'Keolahragaan', 1),
+(39, '426.1', 'Cabang Olah Raga', 'Cabang Olah Raga', 1),
+(40, '426.2', 'Sarana', 'Sarana', 1),
+(41, '426.21', 'Gedung Olah Raga', 'Gedung Olah Raga', 1),
+(42, '426.22', 'Stadion', 'Stadion', 1),
+(43, '426.23', 'Lapangan', 'Lapangan', 1),
+(44, '426.24', 'Kolam renang', 'Kolam renang', 1),
+(45, '426.3', 'Pesta Olah Raga, Klasifikasi nya: PON, Porsade, Olimpiade,', 'Pesta Olah Raga, Klasifikasi nya: PON, Porsade, Olimpiade,', 1),
+(46, '426.4', 'KONI', 'KONI', 1),
+(47, '427', 'Kepramukaan Meliputi: Organisasi dan Kegiatan Remaja', 'Kepramukaan Meliputi: Organisasi dan Kegiatan Remaja', 1),
+(48, '428', 'Kepramukaan', 'Kepramukaan', 1),
+(49, '429', 'Pendidikan Kedinasan Untuk Depdagri', 'Pendidikan Kedinasan Untuk Depdagri', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_sett`
+--
+
+CREATE TABLE `tbl_sett` (
+  `id_sett` tinyint(1) NOT NULL,
+  `surat_masuk` tinyint(2) NOT NULL,
+  `surat_keluar` tinyint(2) NOT NULL,
+  `referensi` tinyint(2) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_sett`
+--
+
+INSERT INTO `tbl_sett` (`id_sett`, `surat_masuk`, `surat_keluar`, `referensi`, `id_user`) VALUES
+(1, 100, 5, 10, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_surat_keluar`
+--
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL,
+  `no_agenda` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_surat_keluar`
+--
+
+INSERT INTO `tbl_surat_keluar` (`id_surat`, `no_agenda`, `tujuan`, `no_surat`, `isi`, `kode`, `tgl_surat`, `tgl_catat`, `file`, `keterangan`, `id_user`) VALUES
+(6, 1, 'Rektor IAHN Gde Puja Mataram', '4828/Kw.18.6/PP.01/08/2021', 'Simakrama', 'Surat Keluar', '2021-08-26', '2021-08-27', '', 'Surat Keluar', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_surat_masuk`
+--
+
+CREATE TABLE `tbl_surat_masuk` (
+  `id_surat` int(10) NOT NULL,
+  `no_agenda` int(10) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `asal_surat` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `indeks` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_surat_masuk`
+--
+
+INSERT INTO `tbl_surat_masuk` (`id_surat`, `no_agenda`, `no_surat`, `asal_surat`, `isi`, `kode`, `indeks`, `tgl_surat`, `tgl_diterima`, `file`, `keterangan`, `id_user`) VALUES
+(23, 440, '440/146/PKM-KT/IX/202', 'PUSKESMAS KARANG TALIWANG', 'Vaksinisasi Covid-19', '001', '001', '2021-09-07', '2021-09-07', '744-surat1.pdf', 'Pemberitahuan Vaksinisasi Covid-19', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `nama`, `nip`, `admin`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Suryadi', '198209052008011008', 1),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'USER KEPSEK', '-', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+  ADD PRIMARY KEY (`id_disposisi`);
+
+--
+-- Indeks untuk tabel `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+  ADD PRIMARY KEY (`id_instansi`);
+
+--
+-- Indeks untuk tabel `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  ADD PRIMARY KEY (`id_klasifikasi`);
+
+--
+-- Indeks untuk tabel `tbl_sett`
+--
+ALTER TABLE `tbl_sett`
+  ADD PRIMARY KEY (`id_sett`);
+
+--
+-- Indeks untuk tabel `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- Indeks untuk tabel `tbl_surat_masuk`
+--
+ALTER TABLE `tbl_surat_masuk`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+  MODIFY `id_disposisi` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  MODIFY `id_klasifikasi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_surat_masuk`
+--
+ALTER TABLE `tbl_surat_masuk`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
